66f4be5b353e3001d5595fb27c5edd7ffda8099f
Desmiraldo <45026779+desmiraldo@users.noreply.github.com>
2022-12-08 23:40:44 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 #include<unistd.h>
 #include <sys/socket.h>
 #include<arpa/inet.h>
-#include "config.h"
 
+#include "config.h"
+#include "functions/client_response.h"
 
 #define FROOT "chs.cfg"
 
@@ -14,8 +15,7 @@
 
 
 
-int main(int argc , char *argv[])
-{
+int main(int argc , char *argv[]){
     //Creating socket
     struct config configuration;
     int port;
@@ -25,7 +25,10 @@ int main(int argc , char *argv[])
 
 	int d_socket,c_socket,len;
     struct sockaddr_in server , client;
-	char *message="HTTP/1.1 200 OK\n"
+
+    struct http crequest;
+    char request[1012];
+	char *response="HTTP/1.1 200 OK\n"
                     "Date: Thu, 4 Dec 2022 3:14:55 GMT\n"
                     "Server: c_http_server\n"
                     "Last-Modified: Wed, 18 Jun 2003 16:05:58 GMT\n"
@@ -53,7 +56,7 @@ int main(int argc , char *argv[])
     if(bind(d_socket,(struct sockaddr *)&server , sizeof(server)) >= 0){
         printf("\nSuccess:\nServer is running\n");
     }else{
-        printf("\nError:\nCould not bind!\n");
+        printf("\nError:\nServer failed!\n");
         return(1);
     }
 
@@ -62,12 +65,19 @@ int main(int argc , char *argv[])
     len = sizeof(struct sockaddr_in);
 
     while(c_socket = accept(d_socket, (struct sockaddr *)&client, &len)){
-	    write(c_socket , message , strlen(message));
+        if((read(c_socket,request,255)) > 0){
+            crequest = getreqdata(request);
+        }else{
+            printf("\nError:\nFailed to read client request!\n");
+        }
+
+	    write(c_socket , response , strlen(response));
         close(c_socket);
+        break;
     }
 	
     if(c_socket < 0){
-        printf("Error");
+        printf("\nError:\nClient socket error!\n");
         return 1;
     }
 
