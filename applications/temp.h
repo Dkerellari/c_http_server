fe841e7b7666f9789d4059a0b7fb4d403b4374d9
Desmiraldo <45026779+desmiraldo@users.noreply.github.com>
2023-01-26 05:34:30 +0100

    Add files via upload

--- applications/temp.h
+++ applications/temp.h
@@ -3,7 +3,13 @@
 
 
 char *hello(){
-    return "hello from function in c";
+    return "C_HTTP_SERVER PROJECT";
+};
+char *text(){
+    return "Buliding for fun!<br>Lets Biuld this together!";
+};
+char *author(){
+    return "Author : Desmiraldo Kerellari";
 };
 
 struct functions fnc[MAXF];
@@ -11,6 +17,10 @@ struct functions fnc[MAXF];
 int getready(){
     fnc[0].func = hello;
     strcpy(fnc[0].varname,"var");
+    fnc[1].func = text;
+    strcpy(fnc[1].varname,"text");
+    fnc[3].func = author;
+    strcpy(fnc[3].varname,"author");
 }
 
 
